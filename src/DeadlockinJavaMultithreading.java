8cb6eef104d8f68f91d9ece117877945721218a3
RicardoReyes44 <rickyboy1002@gmail.com>
2021-03-25 19:13:55 -0600

    Se agrego la clase 'Thread2' con sus atributos 's1' y 's2' en la clase y mtodo 'run'

--- src/DeadlockinJavaMultithreading.java
+++ src/DeadlockinJavaMultithreading.java
@@ -23,9 +23,8 @@ public class DeadlockinJavaMultithreading {
 
 }
 
-
 //Java program to illustrate Deadlock
-//in multithreading
+//in multithreading.
 class Util
 {
  // Util class to sleep a thread
@@ -68,25 +67,48 @@ class Shared
  }
 }
 
+
 class Thread1 extends Thread
 {
-    private Shared s1;
-    private Shared s2;
- 
-    // constructor to initialize fields
-    public Thread1(Shared s1, Shared s2)
-    {
-        this.s1 = s1;
-        this.s2 = s2;
-    }
- 
-    // run method to start a thread
-    @Override
-    public void run()
-    {
-        // taking object lock of s1 enters
-        // into test1 method
-        s1.test1(s2);
-    }
+ private Shared s1;
+ private Shared s2;
+
+ // constructor to initialize fields
+ public Thread1(Shared s1, Shared s2)
+ {
+     this.s1 = s1;
+     this.s2 = s2;
+ }
+
+ // run method to start a thread
+ @Override
+ public void run()
+ {
+     // taking object lock of s1 enters
+     // into test1 method
+     s1.test1(s2);
+ }
 }
 
+
+class Thread2 extends Thread
+{
+ private Shared s1;
+ private Shared s2;
+
+ // constructor to initialize fields
+ public Thread2(Shared s1, Shared s2)
+ {
+     this.s1 = s1;
+     this.s2 = s2;
+ }
+
+ // run method to start a thread
+ @Override
+ public void run()
+ {
+     // taking object lock of s2
+     // enters into test2 method
+     s2.test2();
+ }
+}
