389f9514a9ac0b241b288c791503a800d12cf7a6
RicardoReyes44 <rickyboy1002@gmail.com>
2021-03-25 19:07:15 -0600

    Se agrego la clase 'Unit' en el proyecto

--- src/DeadlockinJavaMultithreading.java
+++ src/DeadlockinJavaMultithreading.java
@@ -1,9 +1,43 @@
 
 public class DeadlockinJavaMultithreading {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	 {
+	     // creating one object
+	     Shared s1 = new Shared();
 
-	}
+	     // creating second object
+	     Shared s2 = new Shared();
 
+	     // creating first thread and starting it
+	     Thread1 t1 = new Thread1(s1, s2);
+	     t1.start();
+
+	     // creating second thread and starting it
+	     Thread2 t2 = new Thread2(s1, s2);
+	     t2.start();
+
+	     // sleeping main thread
+	     Util.sleep(2000);
+	 }
+
+}
+
+
+//Java program to illustrate Deadlock
+//in multithreading.
+class Util
+{
+ // Util class to sleep a thread
+ static void sleep(long millis)
+ {
+     try
+     {
+         Thread.sleep(millis);
+     }
+     catch (InterruptedException e)
+     {
+         e.printStackTrace();
+     }
+ }
 }
